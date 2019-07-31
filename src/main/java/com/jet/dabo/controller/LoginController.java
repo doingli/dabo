e1cf7a94adc6d30d9f0346ea84949a11d1623724
doingli <doingli@foxmail.com>
2019-08-01 01:00:18 +0800

    dabo

--- src/main/java/com/jet/dabo/controller/LoginController.java
+++ src/main/java/com/jet/dabo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.jet.dabo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,11 @@ public class LoginController {
 
     @RequestMapping("/")
     String login() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("this is info");
+        logger.error("this is error");
+
         return "user login";
+
     }
 }
