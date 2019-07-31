2cb6f5f4d6eb73e7685cbeba40a669c2136aafec
doingli(李小根) <doingli@tencent.com>
2019-07-31 16:01:39 +0800

    test

---
+++ src/main/java/com/jet/dabo/DaboApplication.java
@@ -0,0 +1,13 @@
+package com.jet.dabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jet/dabo/DaboApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jet.dabo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaboApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
